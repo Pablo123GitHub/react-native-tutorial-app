4bb9c0b4ad909a2332e5203da25e2cc9d015f1c5
Pablo <paul.ladiv123@gmail.com>
2018-08-20 21:07:23 +0100

    customize UdaciSlider component

--- App.js
+++ App.js
@@ -1,17 +1,30 @@
 import React from 'react'
 import {
-    View
+    View, StyleSheet, Text, Slider
 } from 'react-native'
 import AddEntry from "./components/AddEntry";
 
 
 export default class App extends React.Component {
+    state = {
+        value: 0
+    }
 
 
   render() {
     return (
-       <AddEntry/>
+     <AddEntry/>
     );
   }
 }
 
+
+// const styles = StyleSheet.create({
+//     container: {
+//         flex: 1,
+//         marginLeft: 10,
+//         marginRight: 10,
+//         alignItems: 'stretch',
+//         justifyContent: 'center'
+//     }
+// })
