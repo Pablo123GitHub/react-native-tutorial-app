77bc5a81ad5e966d3ea635b9fb567d4cd5561060
Pablo <paul.ladiv123@gmail.com>
2018-08-28 10:52:17 +0100

    add asyncstorage functions to the AddEntry components

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -6,6 +6,8 @@ import UdaciSteppers from './UdaciSteppers'
 import DateHeader from './DateHeader'
 import { Ionicons } from '@expo/vector-icons'
 import TextButton from './TextButton'
+import { submitEntry, removeEntry} from "../utils/api"
+
 
 function SubmitBtn ({ onPress }) {
     return (
@@ -67,10 +69,14 @@ export default class AddEntry extends Component {
 
         this.setState(() => ({ run: 0, bike: 0, swim: 0, sleep: 0, eat: 0 }))
 
+        submitEntry({ key, entry})
+
     }
 
     reset = () => {
         const key = timeToString()
+
+        removeEntry(key)
     }
 
 
